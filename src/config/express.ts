1cb304fe2205484ec2aa6c497fcf0b0049457652
md akib <mohiuddinmostafakamal@gmail.com>
2019-09-14 00:14:11 +0600

    reduced magic string

--- src/config/express.ts
+++ src/config/express.ts
@@ -2,5 +2,8 @@ export default {
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV,
   APP_NAME: process.env.APP_NAME,
+  APP_DESCRIPTION: process.env.APP_DESCRIPTION,
+  APP_CONTACT_EMAIL: process.env.APP_CONTACT_EMAIL,
   API_VERSION: process.env.API_VERSION,
+  API_HOST: process.env.API_HOST,
 };
--- src/main.ts
+++ src/main.ts
@@ -16,33 +16,42 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@src/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as constants from '@src/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable cors
   app.enableCors();
+
   // Config service
   const CONFIG = app.get('ConfigService');
   // App port
   const PORT = CONFIG.get('express.PORT');
   // App name
   const APP_NAME = CONFIG.get('express.APP_NAME');
+  // App desciption
+  const APP_DESCRIPTION = CONFIG.get('express.APP_DESCRIPTION');
+  // Contact email
+  const APP_CONTACT_EMAIL = CONFIG.get('express.APP_CONTACT_EMAIL');
   // Api version
   const API_VERSION = CONFIG.get('express.API_VERSION');
+  // Swagger host
+  const API_HOST = CONFIG.get('express.API_HOST');
+
   // Path prefix
   app.setGlobalPrefix(`api/v${parseInt(API_VERSION)}`);
   // Global validation pipe
   app.useGlobalPipes(new ValidationPipe());
   // Enable swagger
   const options = new DocumentBuilder()
-    .setHost(`localhost:3000`)
+    .setHost(API_HOST)
     .setBasePath(`/api/v${parseInt(API_VERSION)}`)
     .setTitle(APP_NAME)
-    .setDescription('The gigs app API description')
-    .setContactEmail('mohiuddinmostafakamal@gmail.com')
+    .setDescription(APP_DESCRIPTION)
+    .setContactEmail(APP_CONTACT_EMAIL)
     .setVersion(API_VERSION)
     .setLicense('MIT', 'https://choosealicense.com/licenses/mit/')
-    .addTag('GIGS ROUTES')
+    .addTag(constants.GIGS_ROUTES)
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
